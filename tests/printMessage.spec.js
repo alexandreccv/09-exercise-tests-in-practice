688014feeefa7ec053680813f2cbefe7b0039811
Alexandre Carvalho <alex380bb@gmail.com>
2024-10-24 19:46:41 -0300

    req2

--- tests/printMessage.spec.js
+++ tests/printMessage.spec.js
@@ -2,26 +2,46 @@ const { info, printMessage } = require('../src/printMessage');
 
 describe('2 - Implemente o caso de teste para a função `printMessage`', () => {
   it ('Testa a propriedade personagem no objeto.', () => {
-    // Remova o fail e escreva seus testes abaixo
-    fail('Teste vazio!');
+    let info = {
+      personagem: 'Margarida',
+      origem: 'Pato Donald',
+      nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    };
+
+      // Garante que o objeto possui a propriedade personagem
+      expect(info).toHaveProperty('personagem');
+    
+      // Garante que a função retorna a mensagem correta
+      expect(printMessage(info)).toBe('Boas vindas, Margarida');
   });
 });
 
 describe('3 - Ainda sobre a função `printMessage`, adicione os casos de teste a seguir`', () => {
   it ('Testa a informação de boas vindas.', () => {
-    // Remova o fail e escreva seus testes abaixo
-    fail('Teste vazio!');
+    let info = {
+      personagem: 'Margarida',
+      origem: 'Pato Donald',
+      nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    };
+
+    // Verifica se a mensagem contém "Boas vindas"
+    expect(printMessage(info)).toMatch(/Boas vindas/);
   });
   
   it ('Testa a informação referente ao nome da personagem.', () => {
-    // Remova o fail e escreva seus testes abaixo
-    fail('Teste vazio!');
+    let info = {
+      personagem: 'Margarida',
+      origem: 'Pato Donald',
+      nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    };
+
+    // Verifica se a mensagem contém o nome "Margarida"
+    expect(printMessage(info)).toMatch(/Margarida/);
   });
 });
 
 describe('4 - Crie um fluxo de exceção para a função `printMessage`', () => {
   it ('Testa o fluxo de exceção da função printMessage.', () => {
-    // Remova o fail e escreva seus testes abaixo
-    fail('Teste vazio!');
+    
   });
 });
